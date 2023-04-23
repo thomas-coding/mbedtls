97c5d4b04e0e65b53fd428a6da01d361479508bd
Jinping Wu <jinping.wu@verisilicon.com>
2023-04-23 10:37:31 +0800

    add aes cbc decrypt

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ rm -rf ${shell_folder}/tests/crypto/ctest
 gcc -g -static -o tests/crypto/ctest tests/crypto/ctest.c \
     tests/crypto/crypto_data.c tests/crypto/crypto_util.c \
     tests/crypto/crypto_hash_test.c \
+    tests/crypto/crypto_aes_test.c \
 	-L ${shell_folder}/library -lmbedcrypto -lmbedx509 -lmbedtls \
 	-I ${shell_folder}/include
 
--- tests/crypto/crypto_aes_test.c
+++ tests/crypto/crypto_aes_test.c
@@ -98,8 +98,87 @@ void aes_cbc_encrypt_test(void) {
 
 }
 
+void aes_cbc_decrypt_test(void) {
+    enum {
+        block_size = PSA_BLOCK_CIPHER_BLOCK_LENGTH(PSA_KEY_TYPE_AES),
+    };
+    psa_status_t status;
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
+    psa_algorithm_t alg = PSA_ALG_CBC_NO_PADDING;
+    psa_cipher_operation_t operation = PSA_CIPHER_OPERATION_INIT;
+    uint8_t output[block_size];
+    size_t output_len;
+    psa_key_id_t key_id;
+    size_t total_output_len = 0;
+
+    /* Initialize PSA Crypto */
+    status = psa_crypto_init();
+    if (status != PSA_SUCCESS)
+    {
+        printf("Failed to initialize PSA Crypto\n");
+        return;
+    }
+
+    /* Import a key */
+    psa_set_key_usage_flags(&attributes, PSA_KEY_USAGE_DECRYPT);
+    psa_set_key_algorithm(&attributes, alg);
+    psa_set_key_type(&attributes, PSA_KEY_TYPE_AES);
+    psa_set_key_bits(&attributes, 128);
+    status = psa_import_key(&attributes, aes_cbc_128_key, sizeof(aes_cbc_128_key), &key_id);
+    if (status != PSA_SUCCESS) {
+        printf("Failed to import a key\n");
+        return;
+    }
+    psa_reset_key_attributes(&attributes);
+
+    /* Decrypt the ciphertext */
+    status = psa_cipher_decrypt_setup(&operation, key_id, alg);
+    if (status != PSA_SUCCESS) {
+        printf("Failed to begin cipher operation\n");
+        return;
+    }
+
+    status = psa_cipher_set_iv(&operation, aes_cbc_128_iv, sizeof(aes_cbc_128_iv));
+    if (status != PSA_SUCCESS) {
+        printf("Failed to set IV\n");
+        return;
+    }
+
+    status = psa_cipher_update(&operation, aes_cbc_128_ciphertext, sizeof(aes_cbc_128_ciphertext),
+                               output, sizeof(output), &output_len);
+    if (status != PSA_SUCCESS) {
+        printf("Failed to update cipher operation\n");
+        return;
+    }
+    total_output_len += output_len;
+
+    status = psa_cipher_finish(&operation, output + output_len,
+                               sizeof(output) - output_len, &output_len);
+    if (status != PSA_SUCCESS) {
+        printf("Failed to finish cipher operation\n");
+        return;
+    }
+    total_output_len += output_len;
+
+    memory_hex_dump("aes cbc decrypted data", output, total_output_len);
+    status =  hex_compare(output, aes_cbc_128_plaintext, total_output_len);
+    if (status < 0)
+        printf(RED"[%-50s] FAIL\n"COLOR_NONE, __func__);
+    else
+        printf(GREEN"[%-50s] SUCCESS\n"COLOR_NONE, __func__);
+
+    /* Clean up cipher operation context */
+    psa_cipher_abort(&operation);
+
+    /* Destroy the key */
+    psa_destroy_key(key_id);
+
+    mbedtls_psa_crypto_free();
+}
+
 void crypto_aes_test(void) {
 #if CONFIG_AES_CBC == 1
     aes_cbc_encrypt_test();
+    aes_cbc_decrypt_test();
 #endif
 }
